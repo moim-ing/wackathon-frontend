463bae74e360c0926a435d399fad28b10db35a2d
jun-0411 <jun411@snu.ac.kr>
2026-02-22 07:14:11 +0900

    🐛Fix signup api error

--- src/api/auth/signup.ts
+++ src/api/auth/signup.ts
@@ -2,6 +2,6 @@ import apiClient from '@/api/apiClient';
 import type { SignUpRequest, SignUpResponse } from '@/types/auth';
 
 export default async function signUp(data: SignUpRequest) {
-  const response = await apiClient.post<SignUpResponse>('/auth/login', data);
+  const response = await apiClient.post<SignUpResponse>('/auth/signup', data);
   return response.data;
 }
