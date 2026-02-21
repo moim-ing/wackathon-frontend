4c08638976b590d3b4138c6c490899c46d76abe8
jun-0411 <jun411@snu.ac.kr>
2026-02-22 04:48:01 +0900

    💄Adapt transparent picon

--- src/components/HostNavBar.tsx
+++ src/components/HostNavBar.tsx
@@ -12,7 +12,7 @@ export default function NavBar() {
     <header className="fixed top-2 inset-x-0 z-50 flex justify-center px-2">
       <nav className="mx-auto w-full max-w-3xl flex justify-between items-center gap-2 px-2 py-2">
         <Link to="/host" className="flex items-center space-x-2">
-          <img src="/muCheckLogo.svg" alt="logo" />
+          <img src="/mucheckPicon.svg" width={40} alt="logo" />
         </Link>
         {isLogin ? (
           <Button
