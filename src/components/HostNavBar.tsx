2993e766b58067fe0837d458424d695d21842eaa
Park Junyoung <bloomwayz@snu.ac.kr>
2026-02-21 22:05:59 +0900

    🎨 Split `NavBar` into `GuestNavBar` & `HostNavBar`

--- src/components/HostNavBar.tsx
+++ src/components/HostNavBar.tsx
@@ -1,12 +1,16 @@
+import { Button } from '@/components/ui/button';
 import { Link } from 'react-router';
 
 export default function NavBar() {
   return (
     <header className="fixed top-2 inset-x-0 z-50 flex justify-center px-2">
       <nav className="mx-auto w-full max-w-3xl flex justify-between items-center gap-2 px-2 py-2">
-        <Link to="/" className="flex items-center space-x-2">
+        <Link to="/host" className="flex items-center space-x-2">
           <img src="/moiming-symbol.svg" alt="logo" />
         </Link>
+        <Button variant="link" className="text-black font-semibold">
+          로그아웃
+        </Button>
       </nav>
     </header>
   );
--- src/layouts/HostLayout.tsx
+++ src/layouts/HostLayout.tsx
@@ -1,7 +1,7 @@
-import NavBar from '@/components/NavBar';
+import NavBar from '@/components/HostNavBar';
 import { Outlet } from 'react-router';
 
-export default function RootLayout() {
+export default function HostLayout() {
   return (
     <main>
       <NavBar />
